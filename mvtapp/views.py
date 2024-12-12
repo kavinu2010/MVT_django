c83ac5a2cab9a7d4df4d4caee8fbfa4d3c447b4d
kavitha <kavinu2010@gmail.com>
2024-12-12 12:16:31 +0100

    tested

--- mvtapp/views.py
+++ mvtapp/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,redirect,get_object_or_404
+from .models import Model
+from .forms import ModelForm
+ 
 # Create your views here.
 def home(request):
-  return HttpResponse('Welcome MVT')
+   model=Model.objects.all()
+   return render(request,'home.html',{'model':model})
+
+def create(request):
+   if request.method=='POST':
+    modelform=ModelForm(request.POST)
+    modelform.save()
+    return redirect(home)
+   
+   else:
+     modelform=ModelForm()
+   return render(request, 'create.html',{'modelform':modelform})
+
+def delete(request, post_id):
+  if request.method=='POST':
+    deleteform=get_object_or_404(Model, id=post_id)
+    deleteform.delete()
+    return redirect(home)
+  else:
+    return redirect(home)
+
+
+ 
